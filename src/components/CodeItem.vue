28c35e838810d1fe08a186802c78d36a9c11812c
Spet <snippet0809@163.com>
2024-05-28 14:17:17 +0800

    降低二维码容错率

--- src/components/CodeItem.vue
+++ src/components/CodeItem.vue
@@ -1,45 +1,45 @@
 <template>
-    <div class="layout" ref="pdfItemElement">
-        <!-- 文字和二维码-->
-        <div class="text-qrcode">
-            <!-- 文字 -->
-            <div class="text">
-                <div><span>{{ props.content.text.charAt(0) }}</span></div>
-                <div><span>{{ props.content.text.charAt(1) }}</span></div>
-            </div>
-            <!-- 二维码 -->
-            <div class="qrcode">
-                <canvas ref="qrcodeCanvas"></canvas>
-            </div>
-        </div>
-
-        <!-- 条形码 -->
-        <div class="barcode">
-            <canvas ref="barcodeCanvas"></canvas>
-        </div>
+  <div class="layout" ref="pdfItemElement">
+    <!-- 文字和二维码-->
+    <div class="text-qrcode">
+      <!-- 文字 -->
+      <div class="text">
+        <div><span>{{ props.content.text.charAt(0) }}</span></div>
+        <div><span>{{ props.content.text.charAt(1) }}</span></div>
+      </div>
+      <!-- 二维码 -->
+      <div class="qrcode">
+        <canvas ref="qrcodeCanvas"></canvas>
+      </div>
     </div>
+
+    <!-- 条形码 -->
+    <div class="barcode">
+      <canvas ref="barcodeCanvas"></canvas>
+    </div>
+  </div>
 </template>
 <script lang="ts">
 
 export class Content {
-    text: string
-    qrcode: string
-    barcode: string
+  text: string
+  qrcode: string
+  barcode: string
 
-    constructor(text: string, qrcode: string, barcode: string) {
-        this.text = text
-        this.qrcode = qrcode
-        this.barcode = barcode
-    }
+  constructor(text: string, qrcode: string, barcode: string) {
+    this.text = text
+    this.qrcode = qrcode
+    this.barcode = barcode
+  }
 }
 </script>
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import {onMounted, ref} from 'vue'
 import QRCode from 'qrcode'
 import JsBarcode from 'jsbarcode'
 
 const props = defineProps<{
-    content: Content
+  content: Content
 }>()
 
 
@@ -49,30 +49,31 @@ const barcodeCanvas = ref()
 
 const multiple = 10
 const style = ref({
-    qrcodeWidth: 10 * multiple
+  qrcodeWidth: 10 * multiple
 })
 
 onMounted(() => {
-    console.log(mmToPx(1))
-    QRCode.toCanvas(qrcodeCanvas.value, props.content.qrcode, {
-        width: mmToPx(style.value.qrcodeWidth),
-        margin: 0
-    })
-    JsBarcode(barcodeCanvas.value, props.content.barcode, {
-        width: mmToPx(0.16 * multiple),
-        height: mmToPx(5 * multiple),
-        margin: 0,
-        displayValue: false
-    })
+  console.log(mmToPx(1))
+  QRCode.toCanvas(qrcodeCanvas.value, props.content.qrcode, {
+    width: mmToPx(style.value.qrcodeWidth),
+    margin: 0,
+    errorCorrectionLevel: 'L'
+  })
+  JsBarcode(barcodeCanvas.value, props.content.barcode, {
+    width: mmToPx(0.16 * multiple),
+    height: mmToPx(5 * multiple),
+    margin: 0,
+    displayValue: false
+  })
 })
 
 function mmToPx(mm: number): number {
-    const dom = document.createElement('div')
-    dom.style.width = '1in'
-    document.body.appendChild(dom)
-    const dpi = dom.offsetWidth
-    document.body.removeChild(dom)
-    return (mm * dpi) / 25.4
+  const dom = document.createElement('div')
+  dom.style.width = '1in'
+  document.body.appendChild(dom)
+  const dpi = dom.offsetWidth
+  document.body.removeChild(dom)
+  return (mm * dpi) / 25.4
 }
 </script>
 
@@ -93,52 +94,52 @@ $content-width: $layout-width - $layout-padding-lr*2;
 $content-height: $layout-height - $layout-padding-ub*2;
 
 .layout {
-    width: $content-width;
-    height: $content-height;
-    padding: $layout-padding-ub $layout-padding-lr;
+  width: $content-width;
+  height: $content-height;
+  padding: $layout-padding-ub $layout-padding-lr;
 
-    margin-right: $space-width - $layout-width;
-    margin-bottom: $space-height - $layout-height;
+  margin-right: $space-width - $layout-width;
+  margin-bottom: $space-height - $layout-height;
 }
 
 .text-qrcode {
-    display: flex;
-    height: $qrcode-width;
-    margin-bottom: $qrcode-margin-bottom;
+  display: flex;
+  height: $qrcode-width;
+  margin-bottom: $qrcode-margin-bottom;
 }
 
 .text {
-    width: $content-width;
-    display: block;
+  width: $content-width;
+  display: block;
 }
 
 .text div {
-    display: flex;
-    height: 50%;
-    font-size: 3mm * $multiple;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  height: 50%;
+  font-size: 3mm * $multiple;
+  justify-content: center;
+  align-items: center;
 }
 
 .qrcode {
-    width: $qrcode-width;
-    height: 100%;
-    display: grid;
-    place-items: center;
+  width: $qrcode-width;
+  height: 100%;
+  display: grid;
+  place-items: center;
 }
 
 .barcode {
-    align-items: center;
-    height: $content-height - $qrcode-width - $qrcode-margin-bottom;
-    justify-content: center;
-    display: flex;
+  align-items: center;
+  height: $content-height - $qrcode-width - $qrcode-margin-bottom;
+  justify-content: center;
+  display: flex;
 }
 
 .barcode canvas {
-    position: absolute;
+  position: absolute;
 }
 
 .text span {
-    margin-right: 5mm;
+  margin-right: 5mm;
 }
 </style>
